99dae2af7ad32bed671a7600452ccc8b61a23e13
Paal A. Bergsmark <paal@paals-gamer-w11>
2023-02-06 13:08:41 +0100

    Norwegian (bokmål) translation, draft 1

--- website/next-i18next.config.js
+++ website/next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["ar", "bn", "ca", "de", "en", "es", "fr", "hu", "ja", "ko", "pt-BR", "ru", "uk-UA", "vi", "zh", "tr"],
+    locales: ["ar", "bn", "ca", "de", "en", "es", "fr", "hu", "ja", "ko", "nb-NO", "pt-BR", "ru", "uk-UA", "vi", "zh", "tr"],
   },
 };
